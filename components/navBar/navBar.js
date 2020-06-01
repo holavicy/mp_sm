2d25eef97a58c33c558bf46af8deb3bd1ed7b252
cmj <819953059@qq.com>
2020-06-01 21:12:35 +0800

    提交

--- components/navBar/navBar.js
+++ components/navBar/navBar.js
@@ -7,100 +7,83 @@ Component({
      * 组件的属性列表
      */
     properties: {
-        background: {
-            type: String,
-            value: 'rgba(255, 255, 255, 1)'
-        },
-        color: {
-            type: String,
-            value: 'rgba(0, 0, 0, 1)'
-        },
-        titleText: {
-            type: String,
-            value: '导航栏'
-        },
-        titleImg: {
-            type: String,
-            value: ''
-        },
-        backIcon: {
-            type: String,
-            value: ''
-        },
-        homeIcon: {
-            type: String,
-            value: ''
-        },
-        fontSize: {
-            type: Number,
-            value: 16
-        },
-        iconHeight: {
-            type: Number,
-            value: 19
-        },
-        iconWidth: {
-            type:Number,
-            value: 58
-        }
+      height: {
+        type: String,
+        value:'100'
+      }, //整个导航栏的高度
+      bg: {
+        type: String,
+        value: ''
+      }, //整个导航栏的背景
+      back: {
+        type: Boolean,
+        value: true
+      }, //是否显示左上角的返回按钮
+      background: {
+        type: String,
+        value: 'rgba(255, 255, 255, 1)'
+      }, //标题的背景色
+      color: {
+        type: String,
+        value: 'rgba(0, 0, 0, 1)'
+      }, //标题的颜色
+      fontSize: {
+        type: Number,
+        value: 16
+      },//标题的字号
+      titleText: {
+        type: String,
+        value: '导航栏'
+      } //标题
     },
     attached: function(){
-        var that = this;
-        that.setNavSize();
-        that.setStyle();
+      var that = this;
+      that.setNavSize();
+      that.setStyle();
     },
     data: {
 
     },
     methods: {
-        // 通过获取系统信息计算导航栏高度
-        setNavSize: function() {
-            var that = this
-                , sysinfo = wx.getSystemInfoSync()
-                , statusHeight = sysinfo.statusBarHeight
-                , isiOS = sysinfo.system.indexOf('iOS') > -1
-                , navHeight;
-            if (!isiOS) {
-                navHeight = 48;
-            } else {
-                navHeight = 44;
-            }
-            that.setData({
-                status: statusHeight,
-                navHeight: navHeight
-            })
-        },
-        setStyle: function() {
-            var that  = this
-                , containerStyle
-                , textStyle
-                , iconStyle;
-            containerStyle = [
-                'background:' + that.data.background
-                ].join(';');
-            textStyle = [
-                'color:' + that.data.color,
-                'font-size:' + that.data.fontSize + 'px'
-            ].join(';');
-            iconStyle = [
-                'width: ' + that.data.iconWidth + 'px',
-                'height: ' + that.data.iconHeight + 'px'
-            ].join(';');
-            that.setData({
-                containerStyle: containerStyle,
-                textStyle: textStyle,
-                iconStyle: iconStyle
-            })
-        },
-        // 返回事件
-        back: function(){
-            wx.navigateBack({
-                delta: 1
-            })
-            this.triggerEvent('back', {back: 1})
-        },
-        home: function() {
-            this.triggerEvent('home', {});
+      // 通过获取系统信息计算导航栏高度
+      setNavSize: function() {
+        var that = this
+            , sysinfo = wx.getSystemInfoSync()
+            , statusHeight = sysinfo.statusBarHeight
+            , isiOS = sysinfo.system.indexOf('iOS') > -1
+            , navHeight;
+        if (!isiOS) {
+            navHeight = 48;
+        } else {
+            navHeight = 44;
         }
+        that.setData({
+            status: statusHeight,
+            navHeight: navHeight
+        })
+      },
+      setStyle: function() {
+        var that  = this
+            , containerStyle
+            , textStyle;
+        containerStyle = [
+            'background:' + that.data.background
+            ].join(';');
+        textStyle = [
+            'color:' + that.data.color,
+            'font-size:' + that.data.fontSize + 'rpx'
+        ].join(';');
+        that.setData({
+          containerStyle: containerStyle,
+          textStyle: textStyle
+        })
+      },
+
+      // 返回事件
+      navBack: function(){
+        wx.navigateBack({
+            delta: 1
+        })
+      }
     }
 })
