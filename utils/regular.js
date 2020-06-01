379dc04f3d12567a6061e39de9522d7d2ebb0945
cmj <819953059@qq.com>
2020-06-01 15:52:52 +0800

    提交

--- utils/regular.js
+++ utils/regular.js
@@ -1,12 +1,15 @@
 
 const regs = {
-  email: /^\w+@[a-z0-9]+\.[a-z]{2,4}$/
+  email: /^\w+@[a-z0-9]+\.[a-z]{2,4}$/,
+  idCard: /(^\d{15}$)|(^\d{17}([0-9]|X)$)/,
+  tel: /^1\d{10}$/
 }
 
 export function test(str, regType){
+
   const reg = regs[regType];
 
-  if(str.test(reg)){
+  if(reg.test(str)){
     return true
   } else {
     return false
