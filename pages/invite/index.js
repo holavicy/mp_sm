8700e53641aa7e3787205558e4354bf7f87a0c7a
cmj <819953059@qq.com>
2020-06-03 22:31:05 +0800

    提交

--- pages/invite/index.js
+++ pages/invite/index.js
@@ -6,8 +6,10 @@ Page({
    * 页面的初始数据
    */
   data: {
+    left:0,
     currIndex:0,
-    list:[
+    list:[],
+    listMock:[
       {
       miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
       posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
@@ -16,13 +18,23 @@ Page({
     {
       miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
       posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
-      posterUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590253732499&di=637083a9debac748cc250ab486acf67a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8d2e43377aec54e741ec1a1689389bfd%2Fd2258013632762d05b126ab6a1ec08fa503dc6d1.jpg"
+      posterUrl: "http://119.45.33.38:8080/images/dd/2.png"
     },
     {
       miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
       posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
-      posterUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590253732499&di=637083a9debac748cc250ab486acf67a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8d2e43377aec54e741ec1a1689389bfd%2Fd2258013632762d05b126ab6a1ec08fa503dc6d1.jpg"
+      posterUrl: "http://119.45.33.38:8080/images/dd/3.png"
     },
+      {
+        miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
+        posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
+        posterUrl: "http://119.45.33.38:8080/images/dd/4.png"
+      },
+      {
+        miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
+        posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
+        posterUrl: "http://119.45.33.38:8080/images/dd/5.png"
+      }
   ]
   },
 
@@ -98,28 +110,51 @@ Page({
     request(url, {}).then(res => {
       wx.hideLoading();
 
-      res.data.data = [
-        {
-        miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
-        posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
-        posterUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590253732499&di=637083a9debac748cc250ab486acf67a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8d2e43377aec54e741ec1a1689389bfd%2Fd2258013632762d05b126ab6a1ec08fa503dc6d1.jpg"
-      },
-      {
-        miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
-        posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
-        posterUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590253732499&di=637083a9debac748cc250ab486acf67a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8d2e43377aec54e741ec1a1689389bfd%2Fd2258013632762d05b126ab6a1ec08fa503dc6d1.jpg"
-      },
-      {
-        miniUrl: "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEJjwoVkYEZomhfLkJB7qGA1PlR1ia2Fr14BCdCXwdXM8PMbOHYicfMQr0fgIKKk82ALYIHwh4ziaJ6Vg/132",
-        posterId: "c333f865-d33a-4531-bd2c-6e9d35d20449",
-        posterUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590253732499&di=637083a9debac748cc250ab486acf67a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8d2e43377aec54e741ec1a1689389bfd%2Fd2258013632762d05b126ab6a1ec08fa503dc6d1.jpg"
-      },
-    ]
+      res.data.data = this.data.listMock;
       if(res && res.data && res.data.code == 200){
         this.setData({
           list: res.data.data
         })
       }
     }) 
+  },
+
+  //滑动事件
+  touchstart: function(event){
+    let startX = event.changedTouches[0].pageX;
+    this.setData({
+      startX: startX
+    })
+  },
+
+  touchend: function(event){
+    let endX = event.changedTouches[0].pageX;
+    let startX = this.data.startX;
+    let currIndex = this.data.currIndex;
+
+    let list = this.data.list;
+    let left = 0;
+
+    if (endX - startX > 50 && currIndex>=1){
+      currIndex--;
+      this.setData({
+        currIndex: currIndex
+      })
+    }
+
+    if (endX - startX < -50 && currIndex < list.length-1) {
+      currIndex++;
+      this.setData({
+        currIndex: currIndex
+      })
+    }
+
+    if (currIndex>0){
+      left =0- (666 * currIndex)
+    }
+
+    this.setData({
+      left: left
+    })
   }
 })
