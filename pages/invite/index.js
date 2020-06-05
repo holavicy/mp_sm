6d90182ad9aec70f23f316457905af43ed223bc4
cmj <819953059@qq.com>
2020-06-05 17:31:52 +0800

    提交

--- pages/invite/index.js
+++ pages/invite/index.js
@@ -1,5 +1,6 @@
 // pages/invite/index.js
 import{login, request} from '../../common/interface'
+var app = getApp();
 Page({
 
   /**
@@ -48,7 +49,17 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.getSystemInfo({
+      success (res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+      }
+    })
   },
 
   /**
@@ -101,11 +112,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function (res) {
-    console.log(res)
-    if (res.from === 'button') {
-      // 来自页面内转发按钮
-      console.log(res.target)
-    }
     return {
       title: '盛米咨询',
       path: '/pages/home/index?id=123'
@@ -166,7 +172,64 @@ Page({
     })
   },
 
-  share: function(){
+  saveImg: function () {
+    var currData =  {
+      "miniUrl":"http://119.45.33.38:8080/images/dd/9.jpg",
+      "posterUrl":"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590253732499&di=637083a9debac748cc250ab486acf67a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8d2e43377aec54e741ec1a1689389bfd%2Fd2258013632762d05b126ab6a1ec08fa503dc6d1.jpg",
+      "posterId":"c333f865-d33a-4531-bd2c-6e9d35d20449"
+  }
+  wx.showLoading({
+    title: '海报生成中',
+  })
 
+    var context = wx.createCanvasContext('canvas');
+
+    var postPath = this.getImgTempPath( currData.posterUrl );
+    var miniPath = this.getImgTempPath( currData.miniUrl );
+
+    Promise.all([postPath,miniPath]).then( (res) => {
+      context.setFillStyle('#ffffff');
+      context.fillRect(0, 440, 315, 80)
+      
+
+      context.drawImage(res[0], 0, 0, 315, 440);
+      context.drawImage(res[1], 235, 455, 50, 50);
+      context.setFillStyle('#023C6A');
+      context.setFontSize(12);
+      context.fillText('微信扫描二维码查看详情', 30, 484, 315);
+      context.draw();
+      wx.canvasToTempFilePath({
+        x:0,
+        y:0,
+        width:315,
+        height:520,
+        destWidth: 375,
+        destHeight:625,
+        canvasId: 'canvas',
+        quality: 0,
+        success: (res) => {
+          this.setData({
+            img: res.tempFilePath
+          })
+
+          app.saveImage(res.tempFilePath)
+        }
+      })
+    })
+  },
+
+  getImgTempPath: function(url){
+    return new Promise((resolve, reject) => {
+      wx.downloadFile({
+        url: url,
+        success: (res) => {
+          resolve(res.tempFilePath)
+        },
+        fail: (e) => {
+          console.log(e)
+        }
+        
+      })
+    })
   }
 })
